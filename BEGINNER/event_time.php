1aa1e8ef37d8f40fad76a6f6e8374d5bca241f19
Luis <luuis.avelino@gmail.com>
2021-10-24 21:04:55 -0300

    Atualização do Event_time

--- BEGINNER/event_time.php
+++ BEGINNER/event_time.php
@@ -24,23 +24,31 @@ $H2 = $ultimoDia[0];
 $M2 = $ultimoDia[1];
 $S2 = $ultimoDia[2];
 
+$emprestimoSegundo = 0;
+$emprestimoMinuto = 0;
+$emprestimoHora = 0;
+
+
 
 $segundos = $S2 - $S1;
-if($segundos<0)
-    $segundos += 60;
+if($segundos<0){
+    $segundos = 60 + $S2 - $S1;
+    $emprestimoSegundo += 1;
+}
 
-$minutos = $M2 - $M1;
-if($segundos>0)
-    $minutos += 59;
-    
-$horas = $H2 - $H1;
-if($minutos>0)
-    $horas += 23;
+$minutos = ($M2-$emprestimoSegundo) - $M1;
+if($minutos<0){
+    $minutos = 60 + ($M2-$emprestimoSegundo) - $M1;
+    $emprestimoMinuto += 1;
+}
 
-$dias = $D2 - $D1;
-if (($horas)>0)
-        $dias -= 1;
-    
+$horas = ($H2-$emprestimoMinuto) - $H1;
+if($horas<0){
+    $horas = 24 + ($H2-$emprestimoMinuto) - $H1;
+    $emprestimoHora += 1;
+}
+$dias = ($D2-$emprestimoHora) - $D1;
+
 
 echo "$dias dia(s)\n";
 echo "$horas hora(s)\n";
